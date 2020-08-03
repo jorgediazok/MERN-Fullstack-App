ec50fa063852b92e0ff360a4295d3b282643fef0
Jorge <jorgediazok@gmail.com>
2020-08-03 00:17:41 -0300

    UserPlaces page working ok

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
-import './PlaceList.';
+import './PlaceList.css';
 
 const PlaceList = (props) => {
   if (props.items.length === 0) {
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous skycrappers in the world',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRjpMxbZGv33BVQH0rn6tlATiJx3PBXpk0tag&usqp=CAU',
+    address: '20 W 34th St, New York, NY 10001, Estados Unidos',
+    location: {
+      lat: 40.7484405,
+      long: -73.9878584,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous skycrappers in the world',
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRjpMxbZGv33BVQH0rn6tlATiJx3PBXpk0tag&usqp=CAU',
+    address: '20 W 34th St, New York, NY 10001, Estados Unidos',
+    location: {
+      lat: 40.7484405,
+      long: -73.9878584,
+    },
+    creator: 'u2',
+  },
+];
+
 const UserPlaces = () => {
-  return;
+  return <PlaceList items={DUMMY_PLACES} />;
 };
 
 export default UserPlaces;
