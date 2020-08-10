cfae3767f51eec7b2ed33921b841e9a3d04c3b0c
Jorge <jorgediazok@gmail.com>
2020-08-10 03:47:25 -0300

    refactored code auth

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -19,14 +19,19 @@ export const useHttpClient = () => {
           signal: httpAbortCtrl.signal,
         });
         const responseData = await response.json();
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
         if (!response.ok) {
           throw new Error(responseData.message);
         }
+        setIsLoading(false);
         return responseData;
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
+        throw err;
       }
-      setIsLoading(false);
     },
     []
   );
