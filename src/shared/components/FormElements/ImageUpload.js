a07954432a40b394ac329a89c701f54cd5225d6d
Jorge <jorgediazok@gmail.com>
2020-08-12 00:07:19 -0300

    adding more code into the image upload

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
+import Button from './Button';
 import './ImageUpload.css';
 
-const ImageUpload = (props) => {};
+const ImageUpload = (props) => {
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
+
+  const filePickerRef = useRef();
+
+  const pickedHandler = (event) => {};
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        ref={filePickerRef}
+        style={{ display: 'none' }}
+        type="file"
+        accept=".jpg, .png, .jpeg"
+        onChange={pickImageHandler}
+      />
+      <div className={`image-upload ${props.center && 'center'}`}>
+        <div className="image-upload__preview">
+          <img src="" alt="Preview" />
+        </div>
+        <Button type="button" onClick={pickImageHandler}>
+          PICK IMAGE
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default ImageUpload;
